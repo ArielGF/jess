15b724781c744894d9ca617cf4294ff13828ef09
Ariel <arielgonzalezfernandez21@gmail.com>
2024-11-23 10:40:45 +0100

    Correcciones schema home

--- wp-content/themes/jessicagomez/page-home.php
+++ wp-content/themes/jessicagomez/page-home.php
@@ -1,3 +1,36 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Jessica Gómez",
+      "gender": "Female",
+      "homeLocation": "Gijón",
+      "jobTitle": "Writer",
+      "hasOccupation": {
+        "@type": "Occupation",
+        "name": "Writer"
+        }
+    }
+</script>
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "WebApplication",
+      "name": "Jessica Gómez. Sitio oficial de la autora",
+      "applicationCategory": "Blog",
+      "author": {
+            "@type": "Person",
+            "name": "Ariel González Fernández",
+            "birthDate": "1998-02-21",
+            "birthPlace": {
+                "@type": "Place",
+                "address": "Avilés, Asturias, España"
+            },
+            "jobTitle": "Web developer"
+        }
+    }
+</script>
+
 <?php 
     include get_template_directory() . '/components/portada/portada.php';
     include get_template_directory() . '/components/bloque-libros/bloque-libros.php';
